555bda98132db0ee71a0a1bfef003265f8b979ba
Arturo Villalobos <arturovillgon@hotmail.com>
2021-08-31 17:19:57 -0500

    estilos de input de texto

---
+++ lib/src/widgets/itemDataInput.dart
@@ -0,0 +1,34 @@
+import 'package:demo_openpay/src/widgets/styles/styles.dart';
+import 'package:flutter/material.dart';
+
+
+
+Widget inputTextRegistro(String placeHolder, TextInputType typeInput, bool isPassword, TextEditingController control, [String texto = '', IconData? icono]) {
+  return Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: <Widget>[
+      texto != '' 
+      ? Text( texto,
+        style: labelBoldAzul,
+      )
+      : SizedBox(),
+      
+      SizedBox(height: 20.0),
+      Container(
+        alignment: Alignment.centerLeft,
+        height: 35.0,
+        child: TextField(
+          obscureText: isPassword,
+          keyboardType: typeInput,
+          controller: control,
+          //style: textInputStyle,
+          decoration: InputDecoration(
+            hintText: placeHolder,
+            hintStyle: labelGris,
+          ),
+        ),
+      ),
+    ],
+  );
+}
+
---
+++ lib/src/widgets/styles/styles.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+
+final azulDeep= Color(0xFF000d6e);
+final azul    = Color(0xFF0317ad);
+final azulLow = Color(0xFF6994f5);
+final naranja = Color(0xFFFD9027);
+final rojo    = Color(0xFFC70E0E);
+const verde   = Color(0xFF0EC720);
+const gris    = Color(0xFFABABAB);
+final grisLow = Color(0xFFededed);
+final amarillo= Color(0xFFe3c819);
+
+
+final labelBoldAzul = TextStyle(
+  color: azul,
+  fontFamily: 'Montserrat',
+  fontSize: 14.0,
+  fontWeight: FontWeight.bold,
+);
+
+
+
+final textInputStyle = TextStyle(
+  fontSize: 18,
+  color: azulDeep,
+  fontFamily: 'Montserrat',
+  fontWeight: FontWeight.bold
+);
+
+
+final labelGris = TextStyle(
+  color: gris,
+  fontFamily: 'Montserrat',
+  fontSize: 18.0,
+  fontWeight: FontWeight.normal,
+);
+
