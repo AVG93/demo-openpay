194e52ba2c3a56d22d22880f81116042b45668ae
Arturo Villalobos <arturovillgon@hotmail.com>
2021-08-31 17:20:19 -0500

    Se genera formulario de alta de cliente

--- lib/src/pages/cliente/ClienteAltaPage.dart
+++ lib/src/pages/cliente/ClienteAltaPage.dart
@@ -1,5 +1,6 @@
 import 'package:demo_openpay/src/api/ClienteService.dart';
 import 'package:demo_openpay/src/models/Cliente.dart';
+import 'package:demo_openpay/src/widgets/itemDataInput.dart';
 import 'package:flutter/material.dart';
 
 
@@ -11,23 +12,96 @@ class ClienteAltaPage extends StatefulWidget {
 }
 
 class _ClienteAltaPageState extends State<ClienteAltaPage> {
-  @override
-  Widget build(BuildContext context) {
 
+  void onClickAltaCliente(Cliente c){
     ClienteService clienteService = new ClienteService();
 
-    clienteService.postCliente(new Cliente(id: '', name: 'Username test', email: 'email@correo.com', creationDate: new DateTime(1990, 01, 01)))
+    clienteService.postCliente(c)
     .then((c) => {
       print('${c.id} - ${c.name}')
     });
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+
+    
+
+    TextEditingController _name = TextEditingController();
+    TextEditingController _lastName = TextEditingController();
+    TextEditingController _email = TextEditingController();
+    TextEditingController _phone = TextEditingController();
+    TextEditingController _line1 = TextEditingController();
+    TextEditingController _line2 = TextEditingController();
+    TextEditingController _postalCode = TextEditingController();
+    TextEditingController _state = TextEditingController();
+    TextEditingController _city = TextEditingController();
+    TextEditingController _countryCode = TextEditingController();
+
+    /**/
 
 
     return Scaffold(
       appBar: AppBar(
         title: Text('Alta de Cliente'),
-        
+        actions: [
+          IconButton(
+            onPressed: (){
+
+              onClickAltaCliente(new Cliente(
+                id: '', 
+                name: _name.text, 
+                lastName: _lastName.text,
+                email: _email.text, 
+                phoneNumber: _phone.text,
+                address: new Address(
+                  line1: _line1.text, 
+                  line2: _line2.text, 
+                  state: _state.text, 
+                  city: _city.text, 
+                  postalCode: _postalCode.text, 
+                  countryCode: _countryCode.text),
+                creationDate: new DateTime(1990, 01, 01),
+
+                )
+              );
+
+            }, 
+            icon: Icon(Icons.check),
+            disabledColor: Colors.red,
+          )
+        ],
+      ),
+      body: Container(
+        padding: EdgeInsets.symmetric(horizontal: 20.0),
+        child: Column(
+          
+          children: [
+
+            inputTextRegistro('Nombre', TextInputType.name, false, _name),
+
+            inputTextRegistro('Apellidos', TextInputType.name, false, _lastName),
+
+            inputTextRegistro('Email', TextInputType.emailAddress, false, _email),
+
+            inputTextRegistro('Telefono', TextInputType.phone, false, _phone),
+
+
+
+            inputTextRegistro('Calle', TextInputType.text, false, _line1),
+            inputTextRegistro('Colonia', TextInputType.text, false, _line2),
+            inputTextRegistro('Codigo Postal', TextInputType.number, false, _postalCode),
+            inputTextRegistro('Ciudad', TextInputType.text, false, _city),
+            inputTextRegistro('Estado', TextInputType.text, false, _state),
+            inputTextRegistro('Pais Code', TextInputType.text, false, _countryCode),
+            
+          ],
+        ),
       ),
 
     );
   }
+
+  
 }
