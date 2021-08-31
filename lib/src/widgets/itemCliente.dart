62e4dcf639114013a26d43d603809ce52c9cb517
Arturo Villalobos <arturovillgon@hotmail.com>
2021-08-31 14:45:19 -0500

    creacion de widget de listado de clientes

---
+++ lib/src/widgets/itemCliente.dart
@@ -0,0 +1,50 @@
+
+
+import 'package:demo_openpay/src/models/Cliente.dart';
+import 'package:flutter/material.dart';
+
+Widget itemCliente(BuildContext context, Cliente c, Function onClick){
+    final _screenSize = MediaQuery.of(context).size;
+    
+    Icon icon = Icon(Icons.place, color: Colors.white,);
+    Color colorContent = Colors.green;
+    
+    if(c.address == null){
+      colorContent = Color(0xFFFF5555);
+    }
+
+    Widget line_1 = Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: <Widget>[        
+        Text(c.email)
+      ],
+    );
+
+    Widget line_2 = Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: <Widget>[        
+        c.address != null ? Text(c.address!.city) : Text('NA'),
+        Text(c.creationDate.toString().substring(5, 16))
+      ],
+
+    );
+    
+
+    return ListTile(
+      leading: ClipRRect(
+        borderRadius: BorderRadius.circular(100.0),
+        child: Container(
+          width: _screenSize.width*.1,
+          height: _screenSize.width*.1,
+          color: colorContent,
+          child: icon,
+        ),
+      ),
+      title: line_1,
+      subtitle: line_2,
+      onTap: (){
+        onClick(c);
+      },
+    );
+  }
+
